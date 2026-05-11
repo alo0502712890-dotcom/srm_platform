a905051823cd427e83e094a1ce9f38d467a883e9
Алена Бенедюк <alo0502712890@gmail.com>
2026-05-11 22:05:12 +0300

    Створено модель Task та оновлено UI CRM

--- srm_app/models.py
+++ srm_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('todo', 'До виконання'),
+        ('in_progress', 'В процесі'),
+        ('review', 'На перевірці'),
+        ('done', 'Виконано'),
+    ]
+
+    PRIORITY_CHOICES = [
+        ('low', 'Низький'),
+        ('medium', 'Середній'),
+        ('high', 'Високий'),
+        ('urgent', 'Терміновий'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='low')
+
+    assignee = models.CharField(max_length=100)
+
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
